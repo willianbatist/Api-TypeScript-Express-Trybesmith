7f5e4a1ba01b0d188c960e291bebbf5190fc4d5a
Willian <willian.alves.b15@gmail.com>
2022-06-09 16:24:16 -0300

    feat: req01 complete

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import ProductsController from './controllers/product.controller';
 
 const app = express();
+const productsController = new ProductsController();
 
 app.use(express.json());
+
+app.get('/products', productsController.getAll);
 
 export default app;
