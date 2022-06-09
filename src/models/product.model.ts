b71bf5d57b229a6f82c28f15dc5a893ccca5b3b0
Willian <willian.alves.b15@gmail.com>
2022-06-09 17:25:21 -0300

    feat: created models create

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,5 +1,4 @@
-import { Pool } from 'mysql2/promise';
-// import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import Product from '../interfaces/product';
 
 export default class ProductModel {
@@ -14,5 +13,16 @@ export default class ProductModel {
       .execute('SELECT * FROM Trybesmith.Products');
     const [rows] = result;
     return rows as Product[];
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const { name, amount } = product;
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Users (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...product };
   }
 }
