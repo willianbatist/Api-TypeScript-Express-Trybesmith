a0790a560dcbf108730a60d96a52b33c32f1c23f
Willian <willian.alves.b15@gmail.com>
2022-06-09 15:53:59 -0300

    feat: created product model getAll

---
+++ src/models/product.model.ts
@@ -0,0 +1,18 @@
+import { Pool } from 'mysql2/promise';
+// import { Pool, ResultSetHeader } from 'mysql2/promise';
+import Product from '../interfaces/product';
+
+export default class ProductModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Product[]> {
+    const result = await this.connection
+      .execute('SELECT * FROM Trybesmith.Products');
+    const [rows] = result;
+    return rows as Product[];
+  }
+}
