9d8da3d54e51b8a4b0f4fe0dfe61ee76b55c9b0b
Willian <willian.alves.b15@gmail.com>
2022-06-10 16:24:46 -0300

    fix: error of import Product interface

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,6 +1,6 @@
 import connection from '../models/connection';
 import ProductModel from '../models/product.model';
-import Product from '../interfaces/product';
+import { Product } from '../interfaces/product';
 
 class ProductService {
   public model: ProductModel;
