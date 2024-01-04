aefaaef62d1a9b014cfd82038587db6651f8251e
Willian Batista <willian.alves.b15@gmail.com>
2024-01-04 14:50:09 -0300

    Update order.controller.ts

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,7 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import OrderService from '../services/order.service';
 
 export default class OrderController {
-
+  orderService = new OrderService();
 
   getAll = async (_req:Request, res:Response) => {
     const orders = await this.orderService.getAll();
