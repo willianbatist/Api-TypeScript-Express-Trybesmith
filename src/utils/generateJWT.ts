9eb7b3bedb34ab0ef8243a6dda8758990e65c1dd
Willian Batista <willian.alves.b15@gmail.com>
2024-01-11 14:57:10 -0300

    Update generateJWT.ts

--- src/utils/generateJWT.ts
+++ src/utils/generateJWT.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import User from '../interfaces/user';
 
 function generateJWT(payload: Omit<User, 'password'>) {
-
+  const secret = 'password';
   const jwtConfig:object = {
     expiresIn: '8h',
     algorithm: 'HS256',
